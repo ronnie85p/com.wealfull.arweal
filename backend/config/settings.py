3cf9aaf057f79c564dfd8c42a9cdf94fa5d521cc
Ronnie <romagifted@gmail.com>
2026-08-15 00:18:00 +0700

    fixed migrations

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -118,7 +118,10 @@ DATABASES = {
         'PASSWORD': os.environ.get('DB_PASSWORD', ''),
         'HOST': os.environ.get('DB_HOST', 'localhost'),
         'PORT': os.environ.get('DB_PORT', '3306'),
-        'OPTIONS': {'charset': 'utf8mb4'},
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
